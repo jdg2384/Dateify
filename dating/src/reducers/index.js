967367021bdc821ddd2eebff214642960c05e1af
Patrick Glynn <pjglynn57@gmail.com>
2018-03-09 16:47:10 -0700

    added separate reducers for auth, spotify data, and updating user info

--- dating/src/reducers/index.js
+++ dating/src/reducers/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from 'redux';
 import AuthReducer from './AuthReducer';
+import UserFormReducer from './UserFormReducer';
+import UserReducer from './UserReducer';
 
 export default combineReducers({
   auth: AuthReducer,
+  formInfo: UserFormReducer,
+  user: UserReducer
 });
