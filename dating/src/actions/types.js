765ef305a5795dd6f08a48903fc90a0979e6522d
Patrick Glynn <pjglynn57@gmail.com>
2018-03-09 16:45:02 -0700

    added redux to form where user can update their info

--- dating/src/actions/types.js
+++ dating/src/actions/types.js
@@ -1,2 +1,4 @@
-
 export const LOGIN_USER_SUCCESS = 'login_user_success';
+export const UPDATE_PROPERTY = 'update_property';
+export const UPDATE_AGE = 'update_age';
+export const RESET_AGE = 'reset_age';
