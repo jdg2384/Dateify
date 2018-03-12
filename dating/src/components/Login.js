2e3e5740aaf817d3bdc4831f376e1c20fa20f856
Patrick Glynn <pjglynn57@gmail.com>
2018-03-12 14:57:51 -0600

    finished re-styling Initial login page

--- dating/src/components/Login.js
+++ dating/src/components/Login.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Image, TouchableHighlight } from 'react-native';
+import { Image, TouchableHighlight, Text, StyleSheet, View } from 'react-native';
 import { Card, CardSection, Button } from './common';
 import { initializeSpotify, login } from '../actions';
 
@@ -12,17 +12,53 @@ class Login extends Component {
 	}
 
   render() {
+    const { container, normalText, button, image } = styles;
+
     return (
-      <Card>
-        <CardSection>
-          <Button onPress={() => this.props.login()}>
-            Login
-          </Button>
-        </CardSection>
-      </Card>
+      <View style={container}>
+        <Text style={normalText}>
+          Spotify Dating
+        </Text>
+        <TouchableHighlight style={button} onPress={() => this.props.login()}>
+          <Image resizeMode={'contain'} style={image} source={require('../assets/login-button-mobile.png')} />
+        </TouchableHighlight>
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'black'
+  },
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 250,
+    height: 45,
+    borderRadius: 64
+  },
+  image: {
+    width: 250,
+    height: 50
+  },
+  normalText: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+    color: 'white'
+  },
+  btnText: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    margin: 10,
+    color: 'white'
+  }
+});
 
 const mapStateToProps = state => {
   const { spotifyInitialized, loggedIn } = state.spotify;
