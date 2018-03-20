56ff2695824edf0d6155123c73752c712440f169
Joseph Grounds <jgrounds@Josephs-MacBook-Pro.local>
2018-03-20 12:32:48 -0600

    User form

--- dating/src/components/UserInfoForm.js
+++ dating/src/components/UserInfoForm.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import { View, Image, Text, ScrollView, Picker, StyleSheet } from 'react-native';
+import { View, Image, ScrollView, StyleSheet, Text, Picker , Slider} from 'react-native';
 import { connect } from 'react-redux';
-import { Form, Item, Label, Input, Button } from 'native-base';
+import { Form, Item, Label, Input, Button,} from 'native-base';
 import { Card, CardSection} from './common';
 
 import TrackList from './TrackList';
@@ -24,42 +24,62 @@ class UserInfoForm extends Component {
   componentWillUnmount(props) {
     userPost()
   }
-  
+  state = {
+    language: "",
+    age: 21,
+  }
+  getVal(val){
+    console.warn(val);
+    }    
   render() {
     return (
+      <ScrollView >
       <Form style={styles.form}>
         <View style={styles.container}>
           <Image style={styles.titleStyle} source={{ uri: this.props.imageURL }}/>
         </View>
-        <Item fixedLabel>
-          <Label>Name</Label>
+        <Item fixedLabel style={styles.margin}>
+          <Label style={styles.marginLabel}>Name</Label>
           <Input type='text' value={this.props.name} onChangeText={(value) => this.props.updateProperty(value)}></Input>
         </Item>
-        <Item fixedLabel>
-          <Label>age</Label>
+        <Item fixedLabel style={styles.margin}>
+          <Label style={styles.marginLabel}>age</Label>
           <Input type='text' value={this.props.age} onChangeText={(value) => this.props.updateProperty({prop:'age', value})}></Input>
         </Item>
-        <Item fixedLabel>
-          <Label style={styles.description}>Description</Label>
-          <Input type='text' value={this.props.description} onChangeText={(value) => this.props.updateProperty({prop:'description', value})}></Input>
+        <Item fixedLabel style={styles.margin}>
+          <Label style={styles.descriptionLabel}>Description</Label>
+          <Input style={styles.description} type='text' value={this.props.description} onChangeText={(value) => this.props.updateProperty({prop:'description', value})}></Input>
         </Item>
-        <Item fixedLabel>
-          <Label>Gender</Label>
-          <Input type='text' value={this.props.gender} onChangeText={(value) => this.props.updateProperty({prop:'gender', value})}></Input>
-        </Item>
-        <Item fixedLabel>
-          <Label>Perfered Match Gender</Label>
-          <Input type='text' value={this.props.desired_gender} onChangeText={(value) => this.props.updateProperty({prop:'desired_gender', value})}></Input>
-        </Item>
-        <Item fixedLabel>
+        <Label style={styles.genderMatch}>Gender Match</Label>
+        <Picker
+        style={styles.marginPicker}
+          selectedValue={this.state.language}
+          onValueChange={(itemValue, itemIndex) => this.setState({language: itemValue})}>
+          <Picker.Item label="Female" value="Female" />
+          <Picker.Item label="Male" value="Male" />
+        </Picker>
+        <View style={styles.containerTwo}>
+        <Label style={styles.margin}>Age Range</Label>
+        <Text style={styles.welcome}>
+          {this.state.age}
+        </Text>  
+        <Slider
+        style={styles.slider}
+         style={{ width: 300 }}
+         step={1}
+         minimumValue={18}
+         maximumValue={71}
+         value={this.state.age}
+         onValueChange={val => this.setState({ age: val })}
+         onSlidingComplete={ val => this.getVal(val)}
+        />
+      </View>
+                 
+        {/* <Item fixedLabel>
           <Label>Age Range</Label>
           <Input type='text' value={this.props.age_range} onChangeText={(value) => this.props.updateProperty({prop:'age_range', value})}></Input>
-        </Item>
-        <Item fixedLabel>
-          <Label>Radius</Label>
-          <Input type='text' value={this.props.radius} onChangeText={(value) => this.props.updateProperty({prop:'radius', value})}></Input>
-        </Item>
-        <Button rounded success>
+        </Item> */}
+        {/* <Button rounded success>
           <Text onPress={() => 
             {
               const { gender, description, desired_gender, age, age_range, radius, name, imageURL, spotifyID, topTracks, topArtists } = this.props
@@ -89,8 +109,9 @@ class UserInfoForm extends Component {
             this.props.userPost(obj)}}>
             Success
           </Text>
-        </Button>
+        </Button> */}
       </Form>
+      </ScrollView>
 
     );
   }
@@ -98,17 +119,27 @@ class UserInfoForm extends Component {
 
 const styles = StyleSheet.create({
   form: {
-    backgroundColor: '#fff', 
+    backgroundColor: '#fff',
+    paddingLeft: -10, 
   },
   description: {
-    height: 40,
+    height: 100,
+  },
+  descriptionLabel:{
+    marginTop: -50,
+    marginLeft: 10,
+  },
+  genderMatch:{
+    color:'#585858',
+    marginLeft: 14,
+    marginTop: 14,
   },
   container: {
     justifyContent: 'center',
     alignItems: 'center',
   },
   titleStyle: {
-      margin:15,
+      marginTop:15,
       height:300,
       width:300,
       borderRadius: 300/2,
@@ -116,6 +147,32 @@ const styles = StyleSheet.create({
       borderColor: "#51ba5e",
       justifyContent: 'center',
       alignItems: 'center'
+  },
+  containerTwo: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#E2E2E2',
+    padding: 25,
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  margin: {
+    marginLeft:0,
+  },
+  marginLabel:{
+    marginLeft:15,
+  },
+  marginPicker: {
+    textAlign: 'left',
   }
 })
 
@@ -123,8 +180,6 @@ const mapStateToProps = state => {
   const { name, imageURL, spotifyID, topTracks, topArtists } = state.spotify;
   const { gender, description, desired_gender, age, age_range, radius } = state.user;
   return { gender, description, desired_gender, age, age_range, radius, name, imageURL, spotifyID, topTracks, topArtists };
-
-  
 };
 
 export default connect(mapStateToProps, { 
